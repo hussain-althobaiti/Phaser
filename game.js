01ba755e484920fe5a13d918071d4bc238e36b22
hussain <hussain4div@gmail.com>
2025-09-22 18:14:18 +0300

    Add responsive scaling and styling to game layout

--- game.js
+++ game.js
@@ -435,6 +435,10 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
     physics: { default: 'arcade', arcade: { gravity: { y: 300 }, debug: false } },
     scene: [StartScene, LevelSelectScene, GameScene, EndScene]
 };
